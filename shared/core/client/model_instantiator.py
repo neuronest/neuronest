6294a44d1cb42e28519f64b1e3b3406eed59fc91
Côme Arvis <come.arvis@neuronest.net>
2023-02-26 12:14:57 +0100

    (update) [shared] Make instantiate method synchronous in model instantiator client

--- shared/core/client/model_instantiator.py
+++ shared/core/client/model_instantiator.py
@@ -1,6 +1,3 @@
-from threading import Thread
-from typing import Optional
-
 import requests
 
 from core.client.base import APIClient
@@ -11,25 +8,17 @@ from core.schemas.model_instantiator import (
 
 
 class ModelInstantiatorClient(APIClient):
-    def __init__(
-        self, key_path: str, host: str, wait_for_server_response: bool = False
-    ):
+    def __init__(self, key_path: str, host: str):
         super().__init__(key_path=key_path, host=host)
-        self.wait_for_server_response = wait_for_server_response
 
-    def instantiate(self, model_name: str) -> Optional[requests.Response]:
+    def instantiate(self, model_name: str) -> requests.Response:
         instantiate_call_parameters = dict(
             resource="instantiate",
             verb="post",
             payload=InstantiateModelInput(model_name=model_name).dict(),
         )
 
-        if self.wait_for_server_response is True:
-            return self.call(**instantiate_call_parameters)
-
-        Thread(target=self.call, kwargs=instantiate_call_parameters).start()
-
-        return None
+        return self.call(**instantiate_call_parameters)
 
     def uninstantiate(self, model_name: str) -> requests.Response:
         return self.call(
