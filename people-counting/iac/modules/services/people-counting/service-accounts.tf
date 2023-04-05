dda20c31547174403694d416106bb840030fa61c
Côme Arvis <come.arvis@neuronest.net>
2023-04-05 17:23:15 +0200

    (feat) [people-counting] Remove former iac module

--- people-counting/iac/modules/services/people-counting/service-accounts.tf
+++
@@ -1,7 +0,0 @@
-# Create a service account
-resource "google_service_account" "people_counting_api_sa" {
-  provider     = google-beta
-  account_id   = "people-counting-api"
-  display_name = "Webapp Service Account"
-  depends_on   = [time_sleep.api_activation_waiting]
-}
