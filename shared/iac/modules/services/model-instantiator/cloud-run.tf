df3c4c1cbebb3c8fb6fe6636dc7f4b4129f0b17f
Côme Arvis <come.arvis@neuronest.net>
2023-04-08 04:03:19 +0200

    (fix) [shared] Fix issues

--- shared/iac/modules/services/model-instantiator/cloud-run.tf
+++ shared/iac/modules/services/model-instantiator/cloud-run.tf
@@ -4,7 +4,7 @@
 # Deploy image to Cloud Run
 resource "google_cloud_run_service" "model_instantiator_api" {
   provider                   = google-beta
-  name                       = var.project_name
+  name                       = var.repository_name
   location                   = var.region
   autogenerate_revision_name = true
   template {
