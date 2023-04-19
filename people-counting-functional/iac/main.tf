87139ef04e9f4cdf96f87db068ab653fd2d08fbd
Guillaume Ligner <guillaume.ligner@neuronest.net>
2023-04-19 16:26:07 +0200

    (feat) wip

---
+++ people-counting-functional/iac/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.25.0"
+    }
+  }
+  backend "gcs" {
+    prefix = "terraform/state"
+  }
+}
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+resource "google_project" "project" {
+  name            = var.project_id
+  project_id      = var.project_id
+  org_id          = var.organization_id
+  billing_account = var.billing_account
+}
+
+module "base" {
+  source                       = "../../shared/iac/modules/services/base"
+  project_id                   = var.project_id
+  region                       = var.region
+  firestore_region             = var.firestore_region
+  project_builder_service_account_name = var.project_builder_service_account_name
+}
+
+module "people_counting" {
+  source                       = "../../shared/iac/modules/services/people-counting"
+  project_id                   = var.project_id
+  region                       = var.region
+  repository_name              = var.repository_name
+  image_name                   = var.image_name
+  webapp_service_account_name  = var.webapp_service_account_name
+  firestore_results_collection = var.firestore_results_collection
+  videos_to_count_bucket       = var.videos_to_count_bucket
+  counted_videos_bucket        = var.counted_videos_bucket
+  model_instantiator_host      = var.model_instantiator_host
+  object_detection_model_name  = var.object_detection_model_name
+  cloud_run_memory             = var.cloud_run_memory
+  cloud_run_cpu                = var.cloud_run_cpu
+  cloud_run_min_scale          = var.cloud_run_min_scale
+  cloud_run_max_scale          = var.cloud_run_max_scale
+}
---
+++ people-counting-functional/iac/variables.tf
@@ -0,0 +1,60 @@
+variable "project_id" {
+  type = string
+}
+variable "organization_id" {
+  type = string
+}
+variable "billing_account" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "zone" {
+  type = string
+}
+variable "firestore_region" {
+  type = string
+}
+variable "state_bucket" {
+  type = string
+}
+variable "repository_name" {
+  type = string
+}
+variable "image_name" {
+  type = string
+}
+variable "webapp_service_account_name" {
+  type = string
+}
+variable "firestore_results_collection" {
+  type = string
+}
+variable "videos_to_count_bucket" {
+  type = string
+}
+variable "counted_videos_bucket" {
+  type = string
+}
+variable "model_instantiator_host" {
+  type = string
+}
+variable "object_detection_model_name" {
+  type = string
+}
+variable "cloud_run_memory" {
+  type = string
+}
+variable "cloud_run_cpu" {
+  type = string
+}
+variable "cloud_run_min_scale" {
+  type = string
+}
+variable "cloud_run_max_scale" {
+  type = string
+}
+variable "project_builder_service_account_name" {
+  type = string
+}
