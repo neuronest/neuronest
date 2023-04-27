7f50cdf527c36583ebefdc31852ba5fc5fbbccda
Guillaume Ligner <guillaume.ligner@neuronest.net>
2023-04-27 16:59:18 +0200

    (feat) wip

--- people-counting-functional/iac/main.tf
+++ people-counting-functional/iac/main.tf
@@ -29,20 +29,20 @@ module "base" {
   project_builder_service_account_name = var.project_builder_service_account_name
 }
 
-module "people_counting_functional" {
-  source                       = "../../shared/iac/modules/services/people-counting-functional"
-  project_id                   = var.project_id
-  region                       = var.region
-  repository_name              = var.repository_name
-  image_name                   = var.image_name
-  webapp_service_account_name  = var.webapp_service_account_name
-  firestore_results_collection = var.firestore_results_collection
-  videos_to_count_bucket       = var.videos_to_count_bucket
-  counted_videos_bucket        = var.counted_videos_bucket
-  model_instantiator_host      = var.model_instantiator_host
-  object_detection_model_name  = var.object_detection_model_name
-  cloud_run_memory             = var.cloud_run_memory
-  cloud_run_cpu                = var.cloud_run_cpu
-  cloud_run_min_scale          = var.cloud_run_min_scale
-  cloud_run_max_scale          = var.cloud_run_max_scale
-}
+#module "people_counting_functional" {
+#  source                       = "../../shared/iac/modules/services/people-counting-functional"
+#  project_id                   = var.project_id
+#  region                       = var.region
+#  repository_name              = var.repository_name
+#  image_name                   = var.image_name
+#  webapp_service_account_name  = var.webapp_service_account_name
+#  firestore_results_collection = var.firestore_results_collection
+#  videos_to_count_bucket       = var.videos_to_count_bucket
+#  counted_videos_bucket        = var.counted_videos_bucket
+#  model_instantiator_host      = var.model_instantiator_host
+#  object_detection_model_name  = var.object_detection_model_name
+#  cloud_run_memory             = var.cloud_run_memory
+#  cloud_run_cpu                = var.cloud_run_cpu
+#  cloud_run_min_scale          = var.cloud_run_min_scale
+#  cloud_run_max_scale          = var.cloud_run_max_scale
+#}
