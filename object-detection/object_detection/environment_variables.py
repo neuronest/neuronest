c5ca8d089e07dfd799d0c8010bee7740b28db120
Guillaume Ligner <guillaume.ligner@neuronest.net>
2023-05-19 13:06:05 +0200

    (feat) wip

--- object-detection/object_detection/environment_variables.py
+++ object-detection/object_detection/environment_variables.py
@@ -4,4 +4,7 @@ GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"
 SERVICE_ACCOUNT_EMAIL = os.environ.get("SERVICE_ACCOUNT_EMAIL")
 TRAINING_IMAGE_NAME = os.environ.get("TRAINING_IMAGE_NAME")
 PROJECT_ID = os.environ.get("PROJECT_ID")
-MODEL_INSTANTIATOR_HOST = os.environ["MODEL_INSTANTIATOR_HOST"]
+REGION = os.environ.get("REGION")
+# MODEL_INSTANTIATOR_HOST represents the linking of two services and cannot be expected
+# to be known as an environment variable in all contexts, such as a train for example
+MODEL_INSTANTIATOR_HOST = os.environ.get("MODEL_INSTANTIATOR_HOST")
