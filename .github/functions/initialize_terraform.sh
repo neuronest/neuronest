413a62f10613a01a61aab1509deec5724aed5525
Guillaume Ligner <guillaume.ligner@neuronest.net>
2023-05-23 18:23:30 +0200

    (feat) wip, try single quotes

--- .github/functions/initialize_terraform.sh
+++ .github/functions/initialize_terraform.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 initialize_terraform() {
-  workspace="$1"
-  bucket="$2"
+  repository_name="$1"
+  workspace="$2"
+  bucket="$3"
 
+  cd $repository_name/iac
   # run terraform init and create the terraform state locally
   terraform init -input=false -backend-config="bucket=$bucket"
   terraform workspace select $workspace || terraform workspace new $workspace
