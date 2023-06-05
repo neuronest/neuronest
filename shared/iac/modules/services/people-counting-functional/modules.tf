612997e2202ac4f618c03d011505119ab5878363
Guillaume Ligner <guillaume.ligner@neuronest.net>
2023-06-05 17:50:10 +0200

    (fix) wip

--- shared/iac/modules/services/people-counting-functional/modules.tf
+++ shared/iac/modules/services/people-counting-functional/modules.tf
@@ -5,12 +5,14 @@ module "object_detection" {
   service_account_name         = var.object_detection_service_account_name
   models_bucket                = var.object_detection_models_bucket
   package_name                 = var.object_detection_package_name
+  repository_name              = var.object_detection_repository_name
 }
 module "model_instantiator" {
   source                       = "../model-instantiator"
   project_id                           = var.project_id
   region                               = var.region
   timezone                             = var.timezone
+  repository_name                      = var.model_instantiator_repository_name
   image_name                           = var.model_instantiator_image_name
   webapp_service_account_name          = var.model_instantiator_webapp_service_account_name
   cloud_scheduler_service_account_name = var.model_instantiator_cloud_scheduler_service_account_name
@@ -37,6 +39,7 @@ module "people_counting" {
   cloud_run_cpu                = var.people_counting_cloud_run_cpu
   cloud_run_min_scale          = var.people_counting_cloud_run_min_scale
   cloud_run_max_scale          = var.people_counting_cloud_run_max_scale
+  repository_name              = var.people_counting_repository_name
 
   model_instantiator_host      = module.model_instantiator.url
   object_detection_model_name  = module.object_detection.model_name
