b55789fb9ee866b0751d00c42cef9bd9d88a597f
Côme Arvis <come.arvis@neuronest.net>
2023-06-13 10:03:22 +0200

    (wip) Wip

--- people-counting/people_counting/download_dataset.py
+++ people-counting/people_counting/download_dataset.py
@@ -9,6 +9,7 @@ from people_counting.config import config
 
 if __name__ == "__main__":
     shutil.rmtree(config.paths.videos_directory, ignore_errors=True)
+
     with tempfile.NamedTemporaryFile() as temporary_file:
         gdown.download(
             config.dataset.google_drive_url, temporary_file.name, quiet=False
@@ -16,7 +17,9 @@ if __name__ == "__main__":
         with zipfile.ZipFile(temporary_file.name, "r") as zip_ref:
             os.makedirs(config.paths.videos_directory, exist_ok=True)
             zip_ref.extractall(config.paths.videos_directory)
+
     dataset_name = os.listdir(config.paths.videos_directory).pop()
+
     for file in os.listdir(os.path.join(config.paths.videos_directory, dataset_name)):
         shutil.move(
             os.path.join(config.paths.videos_directory, dataset_name, file),
