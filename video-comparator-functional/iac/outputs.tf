4b8eede4f2b7d8e1eb946b16bb91a12bb8d090bc
Guillaume Ligner <guillaume.ligner@neuronest.net>
2023-08-22 14:58:10 +0200

    (update) Add the model_instantiator_url in output of the video_comparator_functional

--- video-comparator-functional/iac/outputs.tf
+++ video-comparator-functional/iac/outputs.tf
@@ -2,3 +2,6 @@ output "base_project_builder_sa_key" {
   value     = module.base.project_builder_sa_key
   sensitive = true
 }
+output "model_instantiator_url" {
+  value = module.video_comparator_functional.model_instantiator_url
+}
