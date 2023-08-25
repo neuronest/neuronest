36138ce4c2797b29ab9aa75b86dd285f6c385899
Guillaume Ligner <guillaume.ligner@neuronest.net>
2023-08-25 16:23:58 +0200

    (fix) Use the correct project_id to upload the videos, use the correct service account at the deployed endpoint

--- shared/iac/modules/abstract/online-prediction-model/outputs.tf
+++ shared/iac/modules/abstract/online-prediction-model/outputs.tf
@@ -2,5 +2,5 @@ output "model_name" {
   value = var.model_name
 }
 output "service_account_name" {
-  value = var.service_account_name
+  value = google_service_account.online_prediction_model_sa.name
 }
