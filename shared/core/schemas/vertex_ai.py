aff51412cee34f68530a4eab9188225a0ce6768f
Guillaume Ligner <guillaume.ligner@neuronest.net>
2023-08-28 12:29:55 +0200

    (fix) Use service account email, not just service account name

--- shared/core/schemas/vertex_ai.py
+++ shared/core/schemas/vertex_ai.py
@@ -39,7 +39,7 @@ class ServingDeploymentConfig(BaseModel):
     @classmethod
     def from_labels(cls, config_as_labels: Dict[str, str]) -> ServingDeploymentConfig:
         config_as_labels = {
-            hyphen_to_underscore(key): value if value != "None" else None
+            hyphen_to_underscore(key): value if value != "none" else None
             for key, value in config_as_labels.items()
         }
 
