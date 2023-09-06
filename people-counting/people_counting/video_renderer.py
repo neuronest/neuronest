faabaac25569b1a5d579d2d562f4b663715863b4
Côme Arvis <come.arvis@neuronest.net>
2023-09-06 18:25:54 +0200

    (update) [people-counting] Update codec

--- people-counting/people_counting/video_renderer.py
+++ people-counting/people_counting/video_renderer.py
@@ -35,7 +35,7 @@ class VideoRenderer:
             os.makedirs(os.path.dirname(self.output_path), exist_ok=True)
 
     def _initialize_writer(
-        self, width: int, height: int, codec: str = "h264"
+        self, width: int, height: int, codec: str = "avc1"
     ) -> cv.VideoWriter:
         fourcc = cv.VideoWriter_fourcc(*codec)
 
