206d16c909a21163216e48dbb7427672101565d4
Guillaume Ligner <guillaume.ligner@neuronest.net>
2023-09-25 17:33:18 +0200

    (clean) Remove more elements specific to video-comparator

--- shared/iac/modules/services/object-detection/modules.tf
+++
@@ -1,9 +0,0 @@
-module "object_detection" {
-  source     = "../../abstract/online-prediction-model"
-  project_id = var.project_id
-  region     = var.region
-
-  service_account_name = var.service_account_name
-  models_bucket        = var.models_bucket
-  model_name           = var.model_name
-}
