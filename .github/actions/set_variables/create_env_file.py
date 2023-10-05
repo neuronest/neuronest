35930dad1b7c4aac3f137fbea24059f19ffc4cf5
Côme Arvis <come.arvis@neuronest.net>
2023-10-05 18:30:07 +0200

    (feat) Use branch based behaviour (#43)

--- .github/actions/set_variables/create_env_file.py
+++ .github/actions/set_variables/create_env_file.py
@@ -6,7 +6,7 @@ import os
 import re
 import sys
 from enum import Enum
-from typing import List, Optional, Set
+from typing import Dict, List, Optional, Set
 
 import yaml
 from pydantic import BaseModel
@@ -21,6 +21,7 @@ FUNCTIONAL_REPOSITORIES_PREFIX = "func-"
 DEPENDENT_REPOSITORY_VAR_LINE_NAME = "REPOSITORIES_DEPENDENCIES"
 ARRAY_SEPARATOR = ","
 TERRAFORM_VARIABLES_PREFIX = "TF_VAR_"
+OTHER_VARIABLES_DEFAULT_SECTION = "default"
 
 # create logger
 logger = logging.getLogger(__name__)
@@ -321,10 +322,14 @@ class VariableLine:
         return self.name == self.REPOSITORY_CODE_VARIABLE_NAME
 
 
+class BranchConditionedSection(BaseModel):
+    branch: Dict[str, List[str]]
+
+
 class YamlEnvFile(BaseModel):
     repository_variables: List[str]
     multi_instance_resource_names: List[str]
-    other_variables: List[str]
+    other_variables: BranchConditionedSection
 
     @classmethod
     def read_file(cls, file_path: str) -> YamlEnvFile:
@@ -333,7 +338,15 @@ class YamlEnvFile(BaseModel):
 
         return cls.parse_obj(yaml_data)
 
-    def to_variables_lines(self) -> List[VariableLine]:
+    def to_variables_lines(
+        self,
+        branch_name: Optional[str] = None,
+    ) -> List[VariableLine]:
+        other_variables_section = self.other_variables.branch.get(
+            branch_name or OTHER_VARIABLES_DEFAULT_SECTION,
+            self.other_variables.branch[OTHER_VARIABLES_DEFAULT_SECTION],
+        )
+
         return (
             [
                 VariableLine(line=variable, variable_type=VariableType.REPOSITORY)
@@ -347,7 +360,7 @@ class YamlEnvFile(BaseModel):
             ]
             + [
                 VariableLine(line=variable, variable_type=VariableType.OTHER)
-                for variable in self.other_variables
+                for variable in other_variables_section
             ]
         )
 
@@ -375,8 +388,9 @@ class EnvFile:
 class Repository:
     VARIABLES_FILE_PATH = "iac/variables.yaml"
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, branch_name: Optional[str] = None):
         self.name = name
+        self.branch_name = branch_name
 
     @property
     def yaml_env_file_path(self) -> str:
@@ -391,7 +405,9 @@ class Repository:
     def get_dependency_repositories(self) -> List[Repository]:
         variable_name_value = {
             var_line.name: var_line.value
-            for var_line in self.get_yaml_env_file().to_variables_lines()
+            for var_line in self.get_yaml_env_file().to_variables_lines(
+                branch_name=self.branch_name
+            )
         }
         if (
             dependency_repositories := variable_name_value.get(
@@ -402,7 +418,7 @@ class Repository:
                 ARRAY_SEPARATOR
             )
             return [
-                Repository(name=dependency_repository)
+                Repository(name=dependency_repository, branch_name=self.branch_name)
                 for dependency_repository in dependency_repositories
             ]
 
@@ -413,7 +429,9 @@ class Repository:
         # since it is configuration, we don't test it explicitly
         base_code = [
             var_line
-            for var_line in self.get_yaml_env_file().to_variables_lines()
+            for var_line in self.get_yaml_env_file().to_variables_lines(
+                branch_name=self.branch_name
+            )
             if var_line.is_a_repository_code()
         ][0].value
 
@@ -442,7 +460,9 @@ def get_all_repository_var_lines(
         )
         all_repository_var_lines += dependency_repository_var_lines
 
-    all_repository_var_lines += repository.get_yaml_env_file().to_variables_lines()
+    all_repository_var_lines += repository.get_yaml_env_file().to_variables_lines(
+        branch_name=repository.branch_name
+    )
 
     for var_line in all_repository_var_lines:
         if add_namespace:
@@ -496,6 +516,7 @@ if __name__ == "__main__":
         type=str,
         required=True,
     )
+    parser.add_argument("--branch_name", type=str, required=False, default=None)
     parser.add_argument(
         "--environment_variables_file_path",
         type=str,
@@ -530,12 +551,14 @@ if __name__ == "__main__":
 
     shared_variables_lines = YamlEnvFile.read_file(
         file_path=".github/variables/variables.yaml"
-    ).to_variables_lines()
+    ).to_variables_lines(branch_name=args.branch_name)
     shared_variables_lines_names = set(
         shared_var_line.name for shared_var_line in shared_variables_lines
     )
 
-    main_repository = Repository(name=args.repository_name)
+    main_repository = Repository(
+        name=args.repository_name, branch_name=args.branch_name
+    )
 
     if main_repository.has_yaml_env_file():
         repository_variables_lines = get_all_repository_var_lines(
--- shared/core/schemas/vertex_ai.py
+++ shared/core/schemas/vertex_ai.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Dict, List, Optional, Union
 
 from omegaconf import ListConfig
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, root_validator, validator
 
 from core.utils import hyphen_to_underscore, underscore_to_hyphen
 
@@ -35,10 +35,34 @@ class ServingDeploymentConfig(BaseModel):
     accelerator_type: Optional[str] = None
     accelerator_count: int = 0
 
+    @root_validator
+    # pylint: disable=no-self-argument
+    def validate_accelerator_fields(cls, values: dict) -> dict:
+        """
+        To disable GPUs during serving, the proper configuration is and only is:
+            accelerator_type=None
+            accelerator_count=0
+
+        In the case where accelerator_type=ACCELERATOR_TYPE_UNSPECIFIED, we set it to
+        None, and set accelerator_count to 0.
+        Similarly, if accelerator_count=0, we set accelerator_type to None.
+        """
+        if (
+            values["accelerator_type"] is None
+            or values["accelerator_type"].lower() == "accelerator_type_unspecified"
+        ):
+            values["accelerator_count"] = 0
+            values["accelerator_type"] = None
+
+        if values["accelerator_count"] == 0:
+            values["accelerator_type"] = None
+
+        return values
+
     @classmethod
     def from_labels(cls, config_as_labels: Dict[str, str]) -> ServingDeploymentConfig:
         config_as_labels = {
-            hyphen_to_underscore(key): value if value != "None" else None
+            hyphen_to_underscore(key): value if value.lower() != "none" else None
             for key, value in config_as_labels.items()
         }
 
