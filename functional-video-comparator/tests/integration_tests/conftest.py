2dd85b4e5aa358d2a5c5db58025952fe36e3df71
Guillaume Ligner <guillaume.ligner@neuronest.net>
2023-10-06 17:14:34 +0200

    (update) Adds the new endpoint deployment system during integration tests via the instantiator model

--- functional-video-comparator/tests/integration_tests/conftest.py
+++ functional-video-comparator/tests/integration_tests/conftest.py
@@ -1,45 +1,28 @@
-from typing import Optional
-
 import pytest
-import requests
 from core.client.model_instantiator import ModelInstantiatorClient
 from core.client.video_comparator import VideoComparatorClient
 from core.google.vertex_ai_manager import VertexAIManager
 from tests.environment_variables import (
     GOOGLE_APPLICATION_CREDENTIALS,
+    MODEL_INSTANTIATOR_HOST,
     MODEL_NAME,
     PROJECT_ID,
     REGION,
 )
 
 
-class FakeModelInstantiatorClient(ModelInstantiatorClient):
-    def instantiate(self, model_name: str) -> Optional[requests.Response]:
-        response = requests.Response()
-        response.status_code = "201"
-
-        return response
-
-    def uninstantiate(self, model_name: str) -> requests.Response:
-        response = requests.Response()
-        response.status_code = "202"
-
-        return response
-
-
 @pytest.fixture
 def video_comparator_client():
 
-    fake_model_instantiator_client = FakeModelInstantiatorClient(
-        key_path=GOOGLE_APPLICATION_CREDENTIALS, host="fake_model_instantiator_host"
-    )
-    vertex_ai_manager = VertexAIManager(
-        key_path=GOOGLE_APPLICATION_CREDENTIALS, location=REGION, project_id=PROJECT_ID
-    )
-
     return VideoComparatorClient(
-        vertex_ai_manager=vertex_ai_manager,
-        model_instantiator_client=fake_model_instantiator_client,
+        vertex_ai_manager=VertexAIManager(
+            key_path=GOOGLE_APPLICATION_CREDENTIALS,
+            location=REGION,
+            project_id=PROJECT_ID,
+        ),
+        model_instantiator_client=ModelInstantiatorClient(
+            key_path=GOOGLE_APPLICATION_CREDENTIALS, host=MODEL_INSTANTIATOR_HOST
+        ),
         model_name=MODEL_NAME,
         project_id=PROJECT_ID,
     )
