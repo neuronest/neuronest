f2945dd8374a5a222e7e89e8a93340cef98ddd4a
Guillaume Ligner <guillaume.ligner@neuronest.net>
2023-10-06 15:34:38 +0200

    (refactor) Reorganize the terraform files of the video_comparator module

--- shared/iac/modules/services/video-comparator/outputs.tf
+++ shared/iac/modules/services/video-comparator/outputs.tf
@@ -1,3 +1,3 @@
 output "model_name" {
-  value = var.model_name
+  value = module.video_comparator.model_name
 }
--- shared/iac/modules/services/video-comparator/service_accounts.tf
+++
@@ -1,7 +0,0 @@
-# Create a service account
-resource "google_service_account" "object_detection_sa" {
-  provider     = google-beta
-  account_id   = var.service_account_name
-  display_name = "Object detection service account"
-  depends_on   = [time_sleep.api_activation_waiting]
-}
