5e6349890a947a0631bf9e5b39cb7cd7ce22c617
Côme Arvis <come.arvis@neuronest.net>
2023-11-21 03:05:04 +0100

    (update) Update object detection package following shared package updates

--- object-detection/object_detection/config.py
+++ object-detection/object_detection/config.py
@@ -1,22 +1,4 @@
-import logging
-
+from core.config_overriding import override_config_forbidden_characters
 from omegaconf import OmegaConf
 
-logging.basicConfig(level="INFO")
-logger = logging.getLogger(__name__)
-
-cfg = OmegaConf.load("config.yaml")
-
-for forbidden_character, replacement_character in zip(
-    cfg.bigquery.dataset.forbidden_characters,
-    cfg.bigquery.dataset.replacement_characters_of_forbidden_characters,
-):
-    if forbidden_character not in cfg.bigquery.dataset.name:
-        continue
-    cfg.bigquery.dataset.name = cfg.bigquery.dataset.name.replace(
-        forbidden_character, replacement_character
-    )
-    logger.warning(
-        f"We replace the forbidden character '{forbidden_character}' "
-        f"with '{replacement_character}' for the element bigquery.dataset.name"
-    )
+cfg = override_config_forbidden_characters(OmegaConf.load("config.yaml"))
